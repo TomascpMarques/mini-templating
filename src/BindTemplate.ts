ce80de0dccf3976fd34268bb362ecd7eb8cb2f05
TomascpMarques <tomascpmarques@gmail.com>
2022-03-17 21:08:15 +0000

    Init of BindTemplates for content insetion in html

---
+++ src/BindTemplate.ts
@@ -0,0 +1,8 @@
+/**
+ * This class facilitates the use of state/binding values
+ * in the html, allowing for variable content representation
+ * and definitions of default values.
+ */
+class BindTemplates {
+    constructor() { }
+}
