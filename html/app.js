8360a527bd05bbcea7a35ee6c8d9edb676416875
Tomás Marques <tomascpmarques@gmail.com>
2022-04-20 13:51:26 +0100

    Component work and shares js (1/?)

---
+++ html/app.js
@@ -0,0 +1,54 @@
+const app = newMini({
+    entry: 'app',
+    store: {
+        state1: 1,
+        state2: 4,
+        state3: 'Hello',
+        state5: '#ffbaa0',
+        num1: 0,
+        num2: 0,
+        result: 0,
+    },
+    actions: {
+        ss: {
+            target: '_',
+            do: () => console.log('Something cool'),
+        },
+        sumTwoNumbers: {
+            target: 'result',
+            do: (state, stateResult) => {
+                const value1 = state['num1'];
+                const value2 = state['num2'];
+                console.log(`O valor da appState: ${app.store['result']}`);
+                stateResult = Number(value1) + Number(value2);
+                return stateResult;
+            },
+        },
+        docWrite: {
+            target: '_',
+            do: () => {
+                console.log('Here');
+            },
+        },
+        addOne: {
+            target: 'state1',
+            do: (_, targ) => {
+                return ++targ;
+            },
+        },
+        '2x': {
+            target: 'state2',
+            do: (_, targ) => {
+                return (targ *= 2);
+            },
+        },
+        spaceOutLetters: {
+            target: 'state3',
+            do: (_, targ) => {
+                targ = 'Good Bye';
+                return targ;
+            },
+        },
+    },
+    debug: false,
+});
--- src/CustomTemplateTag.ts
+++ src/CustomTemplateTag.ts
@@ -11,6 +11,33 @@ class CustomElement extends HTMLElement {
 
         let componentIframe = document.createElement('iframe');
         componentIframe.setAttribute('src', componentLocation);
+
+
+        componentIframe.width = "100%";
+        componentIframe.onload = function () {
+            if (componentIframe.contentWindow) {
+                componentIframe.height =
+                    (componentIframe.contentWindow.document.body.scrollHeight as number) +
+                    31 + "px";
+                componentIframe.contentWindow.document.body.style.padding = "0";
+                componentIframe.contentWindow.document.body.style.margin = "0";
+            }
+
+            let script = document.createElement('script');
+
+            script.src = Array.from(componentIframe.ownerDocument.scripts).filter(x => x.src.split('/').pop() == 'mini.js').pop().src as string;
+            componentIframe.contentDocument?.head.append(document.createElement('script').src = );
+
+            script = document.createElement('script');
+
+            script.textContent = Array.from(componentIframe.ownerDocument.scripts).pop()?.textContent as string;
+            componentIframe.contentDocument?.body.append(script);
+        }
+        componentIframe.style.border = "none";
+
+
+
+
         shadowRoot.append(componentIframe);
     }
 }
